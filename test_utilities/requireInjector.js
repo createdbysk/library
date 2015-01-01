42d5813f5b007fa27fdf1fdddf2abaa2e31e7186
Satish Kumar <kumarnotifications@gmail.com>
2014-12-31 19:38:12 -0600

    Update requirejsInjector to work correctly as a library.

--- test_utilities/requireInjector.js
+++ test_utilities/requireInjector.js
@@ -13,12 +13,23 @@ createInjector = function () {
         Squire;
     requirejs = require('requirejs');
     requirejs.config({
-        baseUrl: path.resolve(__dirname, "../.."),
+        // This file gets installed under node_modules/library/test_utilities on the client side.
+        // Therefore, the path has to go up 3 directories to get to the project root.
+        baseUrl: path.resolve(__dirname, "../../.."),
         nodeRequire: require,
         packages: [
+            // If the client of this library has squirejs installed, then expect it under
+            // the node_modules on the client.
             {
                 name: "squirejs",
                 location: "node_modules/squirejs",
+                main: "src/Squire"
+            },
+            // If the client of this library does not have squirejs installed, then expect it under
+            // the node_modules under library.
+            {
+                name: "squirejs",
+                location: "node_modules/library/node_modules/squirejs",
                 main: "src/Squire"
             }
         ]

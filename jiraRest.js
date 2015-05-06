6fc18f078f13e5bb6c98f1dc46ed134eff026694
satvidh <satvidh@gmail.com>
2015-05-05 20:49:07 -0500

    Add test more jiraRest functionality and make the tests pass.

--- jiraRest.js
+++ jiraRest.js
@@ -1,6 +1,31 @@
-define(function () {
-    jiraRest = function () {
+define(['utilities/validateAndThrow.js'], function (validate) {
+    'use strict';
+    var jiraRest,
+        jiraRestInstance,
+        search,
+        isRequired;
+    // Options to pass to validate() to check for presence of a property.
+    // validate will return the message if the required property is not present.
+    isRequired = {presence: {message: 'is required'}};
 
+    search = function () {
+
+    };
+    jiraRestInstance = {
+        search: search
+    };
+    /**
+     * Factory method that returns the jiraRestInstance given
+     * options.
+     *
+     * options must be an object with the following required and optional properties.
+     *
+     * required:
+     *  baseUrl -   The base url of the rest api call.
+     */
+    jiraRest = function (options) {
+        validate(options, {baseUrl: isRequired});
+        return jiraRestInstance;
     };
     return jiraRest;
 });
--- test/jiraRest.test.js
+++ test/jiraRest.test.js
@@ -1,10 +1,12 @@
-//'use strict';
+'use strict';
 var configureInjector,
     expect;
 
 configureInjector = require('../test_utilities/configureInjector');
 expect = require('expect.js');
 describe('jira rest', function () {
+    var jiraRest,
+        injector;
     beforeEach(function (done) {
         injector = configureInjector('..', 'node_modules/squirejs');
         injector
@@ -17,4 +19,31 @@ describe('jira rest', function () {
         expect(jiraRest).to.be.a(Function);
         done();
     });
+    it('should return an object with a search function when \
+invoked with options',
+        function (done) {
+            var options,
+                jiraRestInstance;
+            options = {baseUrl: 'http://test.url'};
+            jiraRestInstance = jiraRest(options);
+            expect(jiraRestInstance).to.have.property('search');
+            expect(jiraRestInstance.search).to.be.a(Function);
+            done();
+        }
+    );
+    it('should expect options to contain baseUrl property',
+        function (done) {
+            var options,
+                jiraRestInstance;
+            options = {};
+            try {
+                jiraRest(options);
+            }
+            catch (err) {
+                console.log('here');
+                expect(err).to.be.ok();
+                done();
+            }
+        }
+    );
 });
